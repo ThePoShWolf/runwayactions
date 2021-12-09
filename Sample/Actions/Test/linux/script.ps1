1b7f7b6522b648ea733383ed347647dc66dca5fe
Anthony Howell <anthony@howell-it.com>
2021-12-09 08:30:00 -0800

    adding Linux support

---
+++ Sample/Actions/Test/linux/script.ps1
@@ -0,0 +1,15 @@
+Write-Host 'Write-Host'
+Write-Output 'Write-Output'
+Get-Process | Format-Table
+Get-Item blah.md
+
+$settings = Get-Content ./settings.json | ConvertFrom-Json
+
+#Start-Process runway.exe -ArgumentList @('-N','-S',$settings.host,'download') -Wait
+
+$zipFiles = Get-ChildItem ./download/*.zip
+foreach ($zip in $zipFiles) {
+    Expand-Archive -Path $zip.FullName -DestinationPath ./Results
+}
+
+Copy-Item ./*.* -Destination ./Results
