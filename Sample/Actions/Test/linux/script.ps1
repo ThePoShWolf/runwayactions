9de20a2e0ca099ea99e54ee1174993621c4494bd
Anthony Howell <anthony@howell-it.com>
2021-12-09 08:48:32 -0800

    trying different calls

--- Sample/Actions/Test/linux/script.ps1
+++ Sample/Actions/Test/linux/script.ps1
@@ -3,13 +3,13 @@ Write-Output 'Write-Output'
 Get-Process | Format-Table
 Get-Item blah.md
 
-$settings = Get-Content ./settings.json | ConvertFrom-Json
+#$settings = Get-Content ./settings.json | ConvertFrom-Json
 
-#Start-Process runway.exe -ArgumentList @('-N','-S',$settings.host,'download') -Wait
+<#Start-Process runway.exe -ArgumentList @('-N','-S',$settings.host,'download') -Wait
 
 $zipFiles = Get-ChildItem ./download/*.zip
 foreach ($zip in $zipFiles) {
     Expand-Archive -Path $zip.FullName -DestinationPath ./Results
-}
+}#>
 
 Copy-Item ./*.* -Destination ./Results
