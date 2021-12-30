c94eb607922e9a545d81be4c09ca488ce19ec6fe
Anthony Howell <anthony@howell-it.com>
2021-12-30 13:20:02 -0800

    adding the csr signing

---
+++ ad/offlinejoin/cs/SignCSR/windows/AD.CS.SignCSR.ps1
@@ -0,0 +1,25 @@
+$settings = Get-Content .\settings.json | ConvertFrom-Json
+$settings
+# dl previous action results
+Invoke-WebRequest -Uri 'https://runwaydownloads.blob.core.windows.net/appdl/runway.exe' -OutFile .\runway.exe
+$outFolder = '.\out'
+if (-not (Test-Path $outFolder)) {
+    New-Item $outFolder -ItemType Directory
+}
+.\runway -N -S $settings.host download --directory $outFolder
+# djoin
+$zips = Get-ChildItem $outFolder\*.zip
+if($zips.Count -gt 1) {
+    Write-Host 'Too many zips.'
+} else {
+    Expand-Archive $zips[0].FullName -DestinationPath $outFolder
+
+    Get-ChildItem $outFolder -Recurse
+    
+    $file = Get-ChildItem $outFolder -Recurse -Filter *.req
+
+    certreq -submit $($file.FullName) .\results\cert.cer
+}
+
+# debug
+Copy-Item .\std.out -Destination .\results
